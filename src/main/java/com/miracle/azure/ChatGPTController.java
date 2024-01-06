6991967e11ef8d813e7b8df9e3c4f21c6fcc8de2
dya-only <dyacode@proton.me>
2024-01-07 00:30:45 +0900

    fix: 503 error

--- src/main/java/com/miracle/azure/ChatGPTController.java
+++ src/main/java/com/miracle/azure/ChatGPTController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chatgpt")
+@RequestMapping(value = "/chatgpt", method = {RequestMethod.POST})
 public class ChatGPTController {
 
     private final ChatGPTService chatGPTService;
