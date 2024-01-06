bee7533aa75c34fbccf2dff3346b92739ad7a084
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-06 15:29:40 +0900

    init

---
+++ src/test/java/com/miracle/MiracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
