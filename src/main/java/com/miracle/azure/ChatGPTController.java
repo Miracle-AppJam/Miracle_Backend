4af4b4680975251b37184dcc71247abe68a89f39
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-07 00:34:23 +0900

    Fix :: Type

--- src/main/java/com/miracle/azure/ChatGPTController.java
+++ src/main/java/com/miracle/azure/ChatGPTController.java
@@ -1,10 +1,7 @@
 package com.miracle.azure;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
