48584019a6222ca209893036454e50abadf45332
dya-only <dyacode@proton.me>
2024-01-06 20:42:05 +0900

    refactor: actions

---
+++ src/main/java/com/miracle/health/HealthController.java
@@ -0,0 +1,17 @@
+package com.miracle.health;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping()
+    public String healthCheck() {
+        return "OK";
+    }
+}
